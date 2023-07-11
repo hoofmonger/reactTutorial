d945fd29bccc68b7d5f0dee1d743568ffefdd181
James Hoofard <james.hoofard.iii@gmail.com>
2023-07-10 20:34:38 -0500

    updating

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
-
-ReactDOM.render()
+const page =(
+    <div>
+        <h1>Hello World</h1>
+        <h3>Lorem Ipsum</h3>
+        <ol>
+            <li>Carpe Diem</li>
+            <li>Vini Vidi Vici</li>
+            <li>Arcane</li>
+            <li>Covid-19</li>
+            <li>Elden RING OH ELDEN RING</li>
+        </ol>
+        </div>
+)
+// document.getElementById('root').append(page)
+ReactDOM.render(page, document.getElementById('root'))
