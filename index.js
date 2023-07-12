e761b853df3838eb3fd64d1efe6ebae0f88414c9
James Hoofard <james.hoofard.iii@gmail.com>
2023-07-11 21:20:45 -0500

    updating

--- index.js
+++ index.js
@@ -1,7 +1,34 @@
-const page =(
-    <div>
-        <h1>Hello World</h1>
-        <h3>Lorem Ipsum</h3>
+
+function Header() {
+    return(<header>Hello World
+            <nav className='nav'>
+                <img src='./1024px-Tokyo_Stabbing.jpg' width='100px'></img>
+                <ul className='nav-items'>
+                    <li>
+                        Pricing
+                    </li>
+                    <li>
+                        About
+                    </li>
+                    <li>
+                        Contact
+                    </li>
+                </ul>
+            </nav>
+        </header>
+)}
+
+
+function Footer(){
+    return(
+        <footer>Hoofard Code</footer>
+    )
+}
+
+function Body (){
+    return(
+        <div>
+        <h1>lorem ipsum</h1>
         <ol>
             <li>Carpe Diem</li>
             <li>Vini Vidi Vici</li>
@@ -10,6 +37,18 @@ const page =(
             <li>Elden RING OH ELDEN RING</li>
         </ol>
         </div>
-)
+    )
+}
+
+function Page() {
+    return( <div>
+        <Header/>
+        <Body/>
+        <Footer/>
+        </div>
+)}
 // document.getElementById('root').append(page)
-ReactDOM.render(page, document.getElementById('root'))
+ReactDOM.render(<Page/>, document.getElementById('root'))
+
+//a function that returns react elements, not capitalizing the my in my component
+//calling header as a function instead of a <html>
